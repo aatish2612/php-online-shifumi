90fceabeb502738e06bc74bbe665e193f95231c5
aatish2612 <aatishakshay26@gmail.com>
2025-06-06 11:51:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,30 +4,27 @@
 
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Online shifumi - Accueil</title>
-    </head>
-    <body>
-        <nav>
-            <ul>
-                <li>
-                    <a href="./">Accueil</a>
-                </li>
-                <li>
-                    <a href="./game.php">Nouvelle partie</a>
-                </li>
-                <li>
-                    <a href="./history.php">Historique des parties</a>
-                </li>
-            </ul>
-        </nav>
-        <main>
-            <h1>Shifumi en ligne</h1>
-        </main>
-        <footer>
-            <p>Online shifumi</p>
-        </footer>
-    </body>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Online Shifumi - Accueil</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="game.php">Nouvelle partie</a></li>
+            <li><a href="history.php">Historique des parties</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <h1>Shifumi en ligne</h1>
+        <p>Prêt à jouer ? Clique sur "Nouvelle partie" pour commencer</p>
+    </main>
+
+    <footer>
+        <p>Online Shifumi</p>
+    </footer>
+</body>
 </html>
